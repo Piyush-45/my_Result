9abb06cd9af1add0bab6b6b222ca78f92f60728e
Piyush Tyagi <tyagipiyush2233@gmail.com>
2023-09-11 14:56:08 +0530

    commit2

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 function validate() {
     const rollNo = document.getElementById("username").value;
     const dob = document.getElementById("dob").value;
-    
-    // Replace "123" and "2003-07-06" with your desired roll number and date of birth values
-    const validRollNo ="2100290110112";
+
+    // Replace "2100290110112" and "2003-07-06" with your valid roll number and date of birth values
+    const validRollNo = "2100290110112";
     const validDOB = "2003-07-06";
-    
+
     if (rollNo === validRollNo && dob === validDOB) {
-        // window.location.href= "result.html"
-        redirectToResultPage()
+        redirectToResultPage();
     } else {
         alert("Invalid roll number or date of birth. Please check and try again.");
         return false; // Prevent form submission
